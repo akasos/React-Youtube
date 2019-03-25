af0611f4505d95ab34e6f280b0f3f74160012069
Austin Stephens <austinmoptop@yahoo.com>
2019-03-24 20:25:24 -0600

    Updates

---
+++ precache-manifest.8d6a9be3dac68300a69152dd6426f275.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "93c869ebed31b46d5c59",
+    "url": "/React-Youtube/static/js/runtime~main.e506b26a.js"
+  },
+  {
+    "revision": "a7ccb324571fec2fb3d8",
+    "url": "/React-Youtube/static/js/main.07792a71.chunk.js"
+  },
+  {
+    "revision": "e053a1da17cbfeebd8e3",
+    "url": "/React-Youtube/static/js/2.9176d4f9.chunk.js"
+  },
+  {
+    "revision": "a7ccb324571fec2fb3d8",
+    "url": "/React-Youtube/static/css/main.342c4c53.chunk.css"
+  },
+  {
+    "revision": "61198d6a91bdca11f19ab30f9e920f46",
+    "url": "/React-Youtube/index.html"
+  }
+];
